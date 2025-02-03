d858ab72aecc9372892f6ee82c8aa8834a58f4d5
Vince Pulido <vince.pulido@jhuapl.edu>
2025-02-02 19:31:58 -0500

    more model docs

--- techtrack/modules/inference/model.py
+++ techtrack/modules/inference/model.py
@@ -110,3 +110,17 @@ class Detector:
 
         # Return these variables in order:
         # return bboxes, class_ids, confidence_scores, class_scores
+
+
+"""
+EXAMPLE USAGE:
+model = Detector()
+
+# Perform object detection on the current frame
+predictions = self.detector.predict(frame)
+
+# Extract bounding boxes, class IDs, confidence scores, and class-specific scores
+bboxes, class_ids, confidence_scores, class_scores = self.detector.post_process(
+    predictions, score_threshold=0.5
+)
+"""
